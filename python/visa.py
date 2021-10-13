2796109725229bc923902f2c60207f96935b5ea1
Manuel M Arizmendi <23641126+marizmendi@users.noreply.github.com>
2021-10-13 17:45:45 -0400

    move variables to constants

--- python/visa.py
+++ python/visa.py
@@ -4,23 +4,23 @@ import requests
 
 WAIT_TIME = 3
 
+# Log in with your username and password in https://ais.usvisa-info.com/es-es/niv/users/sign_in
+# Get the _yatri_session cookie using your prefered browser developer tools
+COOKIE = ""
+
+# Click continue on your existing appointment and get the user_id from the URL: https://ais.usvisa-info.com/es-es/niv/schedule/<user_id>/continue_actions
+USER_ID = ""
+
 def check_visa():
     s = requests.Session()
 
-    # Log in with your username and password in https://ais.usvisa-info.com/es-es/niv/users/sign_in
-    # Get the _yatri_session cookie using your prefered browser developer tools
-    cookie = ""
-
-    # Click continue on your existing appointment and get the user_id from the URL: https://ais.usvisa-info.com/es-es/niv/schedule/<user_id>/continue_actions
-    user_id = ""
-
-    url = "https://ais.usvisa-info.com/en-es/niv/schedule/{}/appointment/days/7.json?appointments[expedite]=false".format(user_id)
+    url = "https://ais.usvisa-info.com/en-es/niv/schedule/{}/appointment/days/7.json?appointments[expedite]=false".format(USER_ID)
     headers={
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:92.0) Gecko/20100101 Firefox/92.0",
         "Accept": "application/json, text/javascript, */*; q=0.01",
     }
 
-    s.cookies.set("_yatri_session",cookie)
+    s.cookies.set("_yatri_session",COOKIE)
 
     resp = s.get(url, headers=headers)
     print(resp.json()[0].get('date'))

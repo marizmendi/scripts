657652c0f4cb41b865767ee2a74d251a891e88be
Manuel M Arizmendi <23641126+marizmendi@users.noreply.github.com>
2022-11-17 12:04:17 -0500

    update headers
    liada maxima

--- python/visa.py
+++ python/visa.py
@@ -14,10 +14,24 @@ USER_ID = ""
 def check_visa():
     s = requests.Session()
 
-    url = "https://ais.usvisa-info.com/en-es/niv/schedule/{}/appointment/days/7.json?appointments[expedite]=false".format(USER_ID)
+    url = "https://ais.usvisa-info.com/en-es/niv/schedule/{}/appointment/days/7.json".format(USER_ID)
     headers={
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:92.0) Gecko/20100101 Firefox/92.0",
         "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Host": "ais.usvisa-info.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Sec-GPC": "1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
     }
 
     s.cookies.set("_yatri_session",COOKIE)

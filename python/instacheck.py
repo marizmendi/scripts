3662837ce5704a7e0103cd920f49c9fb7aec84f6
Manuel M Arizmendi <23641126+marizmendi@users.noreply.github.com>
2023-08-12 09:31:22 -0400

    add notification

--- python/instacheck.py
+++ python/instacheck.py
@@ -1,4 +1,5 @@
 import instaloader
+import requests
 
 USER = "manmar92"
 
@@ -22,9 +23,11 @@ def main():
     for follower in profile.get_followers():
         followers.add(follower.username)
 
-    print(followees-followers)
+    mismatch = followees-followers
 
-
+    if(len(mismatch)):
+        requests.post("https://ntfy.sh/manmar92_mismatch",
+            data=f"Mismatch: {mismatch}".encode(encoding='utf-8'))
 
 if __name__ == "__main__":
     main()
